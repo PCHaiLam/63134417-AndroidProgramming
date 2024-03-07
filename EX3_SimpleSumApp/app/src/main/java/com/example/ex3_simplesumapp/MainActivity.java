efe502575430dfc2ac4e377a47813a649838d19d
Phan Châu Hải Lâm <phanchauhailam15803@gmail.com>
2024-03-07 14:43:58 +0700

    simple sum app
    //

---
+++ EX3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ex3_simplesumapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void XuLyCong(View view) {
+        EditText editTextA = findViewById(R.id.edtA);
+        EditText editTextB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+        Button btnCong = findViewById(R.id.btnCong);
+
+        int soA = Integer.parseInt(editTextA.getText().toString());
+        int soB = Integer.parseInt(editTextB.getText().toString());
+
+        editTextKQ.setText(String.valueOf(soA + soB));
+    }
+}
