0ba8a1975fa6b8915ca533d26caad793e1853a2c
Phan Châu Hải Lâm <phanchauhailam15803@gmail.com>
2024-04-23 15:17:33 +0700

    item layout

--- EX8_RecyclerView/app/src/main/java/com/example/ex6_recyclerview/ItemLandscapeAdapter.java
+++ EX8_RecyclerView/app/src/main/java/com/example/ex6_recyclerview/ItemLandscapeAdapter.java
@@ -1,24 +1,37 @@
 package com.example.ex6_recyclerview;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ItemLandscapeAdapter extends RecyclerView.Adapter<ItemLandscapeAdapter.ItemLandscapeViewHolder> {
+    Context context;
+    ArrayList<LandScape> data;
+
+    public ItemLandscapeAdapter(Context context, ArrayList<LandScape> data) {
+        this.context = context;
+        this.data = data;
+    }
 
     @NonNull
     @Override
     public ItemLandscapeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.landscape_item, parent, false);
+        return new ItemLandscapeViewHolder(view);
     }
     @Override
     public void onBindViewHolder(@NonNull ItemLandscapeViewHolder holder, int position) {
+        LandScape land = data.get(position);
+        holder.textViewLandscape.setText(land.getLandScapeName());
 
     }
     @Override
@@ -30,6 +43,8 @@ public class ItemLandscapeAdapter extends RecyclerView.Adapter<ItemLandscapeAdap
         ImageView imageViewLandscape;
         public ItemLandscapeViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageViewLandscape = itemView.findViewById(R.id.imageLandScape);
+            textViewLandscape = itemView.findViewById(R.id.textLandScape);
         }
     }
 }
